858e33cbc5875883db794bc8ad3dcb5fc90c1d64
MuXiu1997 <muxiu1997@gmail.com>
2021-12-15 17:55:25 +0800

    feat: add rules import/order

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -3,4 +3,26 @@ module.exports = {
     '@antfu/eslint-config-basic',
     'plugin:prettier/recommended',
   ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        'groups': [
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+          'external',
+          'builtin',
+          'object',
+          'unknown',
+          'type',
+        ],
+        'newlines-between': 'always',
+        'alphabetize': {
+          order: 'asc',
+        },
+      },
+    ],
+  },
 }
